d576ef5ea7c6891534ef182b9a305e360d080e5b
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-15 16:22:23 -0500

    update copy of home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,8 +9,16 @@ function Home() {
         alt="cat and dog snuggle in grass"
       />
       <p>
-        Petful was made to help connect folks with all pets. Simply join the
-        line and adopt the next cat or dog available when it is your turn!
+        Petful was made to help connect folks with all pets in need. Too often
+        shelter pets can be neglected because of their breed. When you use
+        Petful, your pet will be random (choose between the eligible cat or dog)
+        but its love will be unconditional!
+      </p>
+      <p>
+        To get started, simply access the adopt page and enter the name and
+        password we have on file for you. After submitting, you will know where
+        you are in line or if it is your turn. When you're up next, the portal
+        will unlock and allow you to choose your pet.
       </p>
     </header>
   )
